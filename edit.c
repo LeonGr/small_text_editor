75497d0930e433f9b23370110976ab6037497bfc
LeonGr <mail+github@leongr.nl>
2021-04-25 10:50:40 +0200

    Add error handling

--- edit.c
+++ edit.c
@@ -1,11 +1,21 @@
+/*** includes ***/
+
 #include <ctype.h>
+#include <errno.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+/*** data ***/
+
 struct termios orig_termios;
 
+/*** terminal ***/
+
+/*
+ * Error handling: prints error and exits
+ */
 void die(const char *s) {
     perror(s);
     exit(1);
@@ -15,7 +25,9 @@ void die(const char *s) {
  * Disable raw mode by setting back saved termios struct
  */
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+        die("tcsetattr");
+    }
 }
 
 /*
@@ -23,29 +35,31 @@ void disableRawMode() {
  */
 void enableRawMode() {
     // Store current terminal attributes in raw
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+        die("tcgetattr");
+    }
 
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
 
-     /*
-         Turn off flags:
-         - ICRNL: turn off translation \r to \n
-         - IXON: turn off <C-q>, <C-s> (stop/resume data transmission to terminal)
-         - BRKINT: stop break condition sending SIGINT
-         - INPCK: disable input parity checking
-         - ISTRIP: stop stripping 8th bit of input byte
+    /*
+    Turn off flags:
+    - ICRNL: turn off translation \r to \n
+    - IXON: turn off <C-q>, <C-s> (stop/resume data transmission to terminal)
+    - BRKINT: stop break condition sending SIGINT
+    - INPCK: disable input parity checking
+    - ISTRIP: stop stripping 8th bit of input byte
 
-         - OPOST: turn off special output processing (\n -> \r\n)
+    - OPOST: turn off special output processing (\n -> \r\n)
 
-         - CS8: set character size to 8 bits per byte
+    - CS8: set character size to 8 bits per byte
 
-         - ECHO: print characters as they are typed
-         - ICANON: canonical mode, only send to program once you press Return or <C-d>
-         - IEXTEN: turn off special input processing (<C-v>)
-         - ISIG: turn off <C-c>, <C-z>
-     */
+    - ECHO: print characters as they are typed
+    - ICANON: canonical mode, only send to program once you press Return or <C-d>
+    - IEXTEN: turn off special input processing (<C-v>)
+    - ISIG: turn off <C-c>, <C-z>
+    */
     raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= ~(CS8);
@@ -57,15 +71,23 @@ void enableRawMode() {
     raw.c_cc[VTIME] = 1;
 
     // Save new terminal attributes
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die("tcsetattr");
+    }
 }
+
+/*** init ***/
 
 int main() {
     enableRawMode();
 
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+            die("read");
+        }
+
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
